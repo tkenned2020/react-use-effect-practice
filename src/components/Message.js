375296de95dbddecaaf1462a1298d7a779dcb11c
tkenned2020 <tkenned2020@outlook.com>
2021-06-09 18:15:50 -0400

    complete project

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,46 @@
-function Message({ size }) {
-    console.log('Message', size);
+import { useEffect, useState } from "react";
 
-    return (
-        <div className="message medium">
-            (Oh my! Your bird is naked!)
-        </div>
-    );
-};
+function Message({ size, featherCount }) {
+  //Wrap each console.log
+  useEffect(() => {
+    console.log("Message", size);
+  }, [size]);
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (featherCount <= 0) setMessage("Oh my! Your bird is naked!");
+    else if (featherCount >= 10) {
+      setMessage("Full turkey!");
+    } else {
+      setMessage("Coming along...");
+    }
+  }, [featherCount]);
+
+  const [sizeClass, setSizeClass] = useState("");
+
+  useEffect(() => {
+    console.log("PictureDisplay size", size);
+    let cname = "";
+    switch (size) {
+      case "m":
+        cname = "medium";
+        break;
+      case "l":
+        cname = "large";
+        break;
+      case "xl":
+        cname = "xlarge";
+        break;
+      default:
+        cname = "small";
+        break;
+    }
+    // console.log(cname)
+    setSizeClass(cname);
+  }, [size]);
+
+  return <div className={`message ${sizeClass}`}>{message}</div>;
+}
 
 export default Message;
-
--- src/components/PictureDisplay.js
+++ src/components/PictureDisplay.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import turkey from "../images/turkey.png";
 import feather1 from "../images/feather1.svg";
 import feather2 from "../images/feather2.svg";
@@ -11,33 +13,68 @@ import feather9 from "../images/feather9.svg";
 import featherA from "../images/featherA.svg";
 
 const feathers = [
-    feather1,
-    feather2,
-    feather3,
-    feather4,
-    feather5,
-    feather6,
-    feather7,
-    feather8,
-    feather9,
-    featherA,
+  feather1,
+  feather2,
+  feather3,
+  feather4,
+  feather5,
+  feather6,
+  feather7,
+  feather8,
+  feather9,
+  featherA,
 ];
 
-function PictureDisplay ({ size, featherCount, featherColors }) {
-  console.log('PictureDisplay', size, featherCount, featherColors);
+function PictureDisplay({ size, featherCount, featherColors }) {
+  // useEffect(() => {
+  //   console.log('PictureDisplay', size, featherCount, featherColors);
+  // }, [size, featherCount, featherColors]);
+
+  // useEffect( ()=> {console.log('PictureDisplay', size, featherCount, featherColors), [size, featherCount, featherColors]);
+
+  const [sizeClass, setSizeClass] = useState('');
+
+  useEffect(() => {
+    console.log("PictureDisplay size", size);
+    let cname = '';
+    switch (size) {
+        case 'm':
+            cname = 'medium';
+            break;
+        case 'l':
+            cname = 'large';
+            break;
+        case 'xl':
+            cname = 'xlarge';
+            break;
+        default:
+            cname = 'small';
+            break;
+    }
+    // console.log(cname)
+    setSizeClass(cname);
+  }, [size]);
+
+  useEffect(() => {
+    console.log("PictureDisplay feather count", featherCount);
+  }, [featherCount]);
+
+  useEffect(() => {
+    console.log("PictureDisplay feather colors", featherColors);
+  }, [featherColors]);
 
   // TODO: Wrap in useEffect
   const colors = [];
-  if (!featherColors || featherColors.length === 0) featherColors = [''];
-  for (let i=0; i<featherCount; i++) {
+  if (!featherColors || featherColors.length === 0) featherColors = [""];
+  for (let i = 0; i < featherCount; i++) {
     colors.push(featherColors[i % featherColors.length]);
   }
 
   return (
-    <div className={`image-area medium`}>
-      {colors.map((c, i) =>
+    <div className={`image-area ${sizeClass}`}>
+      {colors.map((c, i) => (
         <img src={feathers[i]} className={`image-feather ${c}`} alt="" />
-      )}
+      ))}
 
       <img src={turkey} className="image-turkey" alt="turkey" />
     </div>
